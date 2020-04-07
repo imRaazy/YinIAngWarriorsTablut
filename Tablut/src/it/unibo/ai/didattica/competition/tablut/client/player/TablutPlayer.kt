62fb2a65face99ccda27ddec15239f886dea8f85
imRaazy <milo.marchetti@libero.it>
2020-04-07 20:07:50 +0200

    Minor changes in TablutPlayer to allow him to play in order to check heuristic function behaviour

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/TablutPlayer.kt
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/TablutPlayer.kt
@@ -16,22 +16,36 @@ class TablutPlayer(private val role: String?, name: String?, val timeout: Int, i
     override fun run() {
         val initialState: State
         val game: PlayerGame
-//        val search: IterativeDeepeningAlphaBetaSearch<State, Action, State.Turn>
+        val search: IterativeDeepeningAlphaBetaSearch<State, Action, State.Turn>
+        var state: State
         println("You are player $role!")
         // Setting up the initial state and player turn
         initialState = StateTablut()
         initialState.turn = role?.let { State.Turn.valueOf(it.toUpperCase()) }
         println("Current state:\n${initialState}")
         // Setting up the game model
-        game = PlayerGame(initialState,99, 0, "garbage", "placeholder", "placeholder")
-        game.getActions(initialState).forEach { println(it) }
+        game = PlayerGame(
+                initialState,
+                99,
+                0,
+                "garbage",
+                "placeholder",
+                "placeholder"
+        )
         // Setting up the search strategy
-//        search = IterativeDeepeningAlphaBetaSearch(game, Double.MIN_VALUE, Double.MAX_VALUE, timeout)
-//        declareName()
-//        while (true) {
-//            read()
-//            println("Current state:\n$currentState")
-//        }
+        search = IterativeDeepeningAlphaBetaSearch(game, Double.MIN_VALUE, Double.MAX_VALUE, timeout)
+        declareName()
+        while (true) {
+            read()
+            state = currentState
+            println("Current state:\n$state")
+            if (state.turn == player) {
+                val a = search.makeDecision(state)
+                println("Chosen move = $a")
+                write(a)
+            } else {
+                println("Waiting for the opposite move...")
+            }
+        }
     }
-
 }
