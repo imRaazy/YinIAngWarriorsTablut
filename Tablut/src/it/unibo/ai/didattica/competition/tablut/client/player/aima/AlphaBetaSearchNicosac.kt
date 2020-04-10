b70828a6ebea577b2cc8c64c42dc8e3b9929846e
nicosac97 <51447518+nicosac97@users.noreply.github.com>
2020-04-10 12:58:46 +0200

    Add kotlin class AlphaBetaSearchNicosac

---
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/aima/AlphaBetaSearchNicosac.kt
@@ -0,0 +1,33 @@
+package it.unibo.ai.didattica.competition.tablut.client.player.aima
+
+import aima.core.search.adversarial.Game
+import aima.core.search.adversarial.IterativeDeepeningAlphaBetaSearch
+import it.unibo.ai.didattica.competition.tablut.domain.Action
+import it.unibo.ai.didattica.competition.tablut.domain.State
+
+class AlphaBetaSearchNicosac(game: Game<State, Action, State.Turn>?, utilMin: Double, utilMax: Double, time: Int) :
+        IterativeDeepeningAlphaBetaSearch<State, Action, State.Turn>(game, utilMin, utilMax, time) {
+    /**
+     * Heuristic function that evaluate the correctness of the given
+     * state if it is not terminal otherwise return the value of getUtils
+     * @param state
+     *          given state
+     * @param turn
+     *          player role
+     * @return evaluation
+     */
+    override fun eval(state: State, turn: State.Turn): Double {
+        if (game.isTerminal(state))
+            return game.getUtility(state, turn)
+        return if (turn == State.Turn.BLACK) evalBlack(state) else evalWhite(state)
+    }
+
+    private fun evalBlack(state: State): Double {
+        return Double.NEGATIVE_INFINITY
+    }
+
+    private fun evalWhite(state: State): Double {
+        return Double.NEGATIVE_INFINITY
+    }
+    
+}
