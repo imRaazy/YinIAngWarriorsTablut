71dc56f2bdf17f724e03f235b40415fd9cb22413
imRaazy <milo.marchetti@libero.it>
2020-04-13 21:21:07 +0200

    Remove PositionWeight enum

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/heuristic/util/PositionWeight.kt
+++
@@ -1,5 +0,0 @@
-package it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util
-
-enum class PositionWeight (val weight: Int) {
-    WHITEGOODLINE(10), WHITEMEDIUMLINE( 5), KINGENCICLERMENT(1)
-}
