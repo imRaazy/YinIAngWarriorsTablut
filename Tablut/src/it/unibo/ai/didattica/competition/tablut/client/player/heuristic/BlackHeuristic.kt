f663fe884a612512c649f934e0cf2da0f9f21c4c
imRaazy <milo.marchetti@libero.it>
2020-04-14 18:21:21 +0200

    Create general white heuristic function based on the balck one and update black heuristic

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/heuristic/BlackHeuristic.kt
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/heuristic/BlackHeuristic.kt
@@ -2,6 +2,7 @@ package it.unibo.ai.didattica.competition.tablut.client.player.heuristic
 
 import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicElement
 import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil.Companion.blackWin
 import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil.Companion.goodSquare
 import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil.Companion.normalizeValue
 import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil.Companion.winnigSquare
@@ -11,15 +12,13 @@ class BlackHeuristic {
     companion object {
         fun genericBlackEval(state: State): Double {
             val heuristicInfluenceElement = mutableListOf<HeuristicElement>()
-            val numberOfBlack = state.getNumberOf(State.Pawn.BLACK)
-            val numberOfWhite = state.getNumberOf(State.Pawn.WHITE)
+            var numberOfBlack = 0
+            var numberOfWhite = 1
             var kingEncirclement = 0
             var kingPositioning = 0
             var whiteEncirclement = 0
             state.board.indices.forEach { r ->
                 state.board.indices.forEach { c ->
-                    if (state.getPawn(r, c) == State.Pawn.WHITE)
-                        whiteEncirclement += HeuristicUtil.getPawnEncirclement(state, Pair(r, c)) { it == State.Pawn.BLACK }
                     if (state.getPawn(r, c) == State.Pawn.KING) {
                         kingEncirclement += HeuristicUtil.getPawnEncirclement(state, Pair(r, c)) { it == State.Pawn.BLACK }
                         if (r in winnigSquare || c in winnigSquare)
@@ -27,15 +26,21 @@ class BlackHeuristic {
                         else if (r in goodSquare || c in goodSquare)
                             kingPositioning = 1
                     }
+                    if (state.getPawn(r, c) == State.Pawn.WHITE) {
+                        whiteEncirclement += HeuristicUtil.getPawnEncirclement(state, Pair(r, c)) { it == State.Pawn.BLACK }
+                        numberOfWhite++
+                    }
+                    if (state.getPawn(r, c) == State.Pawn.BLACK)
+                        numberOfBlack++
                 }
             }
             heuristicInfluenceElement.add(HeuristicElement("KingEncirclement", kingEncirclement, 0, 4, 0.4))
-            heuristicInfluenceElement.add(HeuristicElement("KingPositioning", kingPositioning, 0, 2, 0.8))
+            heuristicInfluenceElement.add(HeuristicElement("KingPositioning", kingPositioning, 0, 2, 0.6))
             heuristicInfluenceElement.add(HeuristicElement("NumberOfWhite", 1 / numberOfWhite, 0, 1, 0.2))
             //heuristicInfluenceElement.add(HeuristicElement("NumberOfBlack", numberOfBlack, 0, MAXBLACK, 0.2))
             heuristicInfluenceElement.add(HeuristicElement("WhiteEncirclement", whiteEncirclement, 0, numberOfBlack * 2, 0.1))
 
-            return if (kingEncirclement ==  4) Double.POSITIVE_INFINITY
+            return if (blackWin(state)) 1.0
                    else HeuristicUtil.weightedAverage(heuristicInfluenceElement.map { Pair(normalizeValue(it.value, it.min, it.max), it.factor) })
         }
     }
--- Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/heuristic/WhiteHeuristic.kt
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/heuristic/WhiteHeuristic.kt
@@ -1,4 +1,82 @@
 package it.unibo.ai.didattica.competition.tablut.client.player.heuristic
 
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicElement
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil.Companion.getCol
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil.Companion.getKing
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil.Companion.getPawnEncirclement
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil.Companion.getRow
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil.Companion.goodSquare
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil.Companion.whiteWin
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.util.HeuristicUtil.Companion.winnigSquare
+import it.unibo.ai.didattica.competition.tablut.domain.State
+
 class WhiteHeuristic {
+    companion object {
+        fun genericWhiteEval(state: State): Double {
+            val heuristicInfluenceElement = mutableListOf<HeuristicElement>()
+            val kingPosition = getKing(state)!!
+            var numberOfBlack = 0
+            var numberOfWhite = 1
+            var kingEncirclement = getPawnEncirclement(state, kingPosition) { it == State.Pawn.WHITE }
+            var blackEncirclement = 0
+            when (kingEncirclement) {
+                4 -> kingEncirclement = 0
+                3 -> kingEncirclement = 1
+                1 -> kingEncirclement = 3
+            }
+            state.board.indices.forEach { r ->
+                state.board.indices.forEach { c ->
+                    if (state.getPawn(r, c) == State.Pawn.WHITE) {
+                        blackEncirclement += getPawnEncirclement(state, Pair(r, c)) { it == State.Pawn.BLACK }
+                        numberOfWhite++
+                    }
+                    if (state.getPawn(r, c) == State.Pawn.BLACK)
+                        numberOfBlack++
+                }
+            }
+            heuristicInfluenceElement.add(HeuristicElement("KingPositioning", evaluateKingPositioning(kingPosition, state), 0, 4, 0.5))
+            heuristicInfluenceElement.add(HeuristicElement("KingEncirclement", kingEncirclement, 0, 3, 0.3))
+            //heuristicInfluenceElement.add(HeuristicElement("NumberOfWhite", numberOfWhite, 0, MAXWHITE, 0.2))
+            heuristicInfluenceElement.add(HeuristicElement("NumberOfBlack", 1 / numberOfBlack, 0, 1, 0.2))
+            heuristicInfluenceElement.add(HeuristicElement("BlackEncirclement", blackEncirclement, 0, numberOfWhite*2, 0.1))
+            return if (whiteWin(kingPosition)) 1.0
+                   else HeuristicUtil.weightedAverage(heuristicInfluenceElement.map { Pair(HeuristicUtil.normalizeValue(it.value, it.min, it.max), it.factor) })
+        }
+        private fun evaluateKingPositioning(kingPosition: Pair<Int, Int>, state: State): Int {
+            var kingPositioning = 0
+            if (kingPosition.first in winnigSquare) {
+                kingPositioning += 2
+                if (getRow(kingPosition.first, state).substringBefore("K").contains("B") ||
+                    getRow(kingPosition.first, state).substringAfter("K").contains("B"))
+                    kingPositioning--
+            }
+            if (kingPosition.second in winnigSquare) {
+                kingPositioning += 2
+                if (getCol(kingPosition.first, state).substringBefore("K").contains("B") ||
+                    getCol(kingPosition.first, state).substringAfter("K").contains("B"))
+                    kingPositioning--
+            }
+            if (kingPosition.first in goodSquare) {
+                kingPositioning += 1
+                if (kingPosition.first < 4)
+                    if (getCol(kingPosition.first, state).substringBefore("K").contains("B"))
+                        kingPositioning--
+                    else
+                        if (getCol(kingPosition.first, state).substringAfter("K").contains("B"))
+                            kingPositioning--
+            }
+            if (kingPosition.second in goodSquare) {
+                kingPositioning += 1
+                if (kingPosition.first < 4)
+                    if (getCol(kingPosition.first, state).substringBefore("K").contains("B"))
+                        kingPositioning--
+                    else
+                        if (getCol(kingPosition.first, state).substringAfter("K").contains("B"))
+                            kingPositioning--
+            }
+            return kingPositioning
+        }
+    }
+
 }
