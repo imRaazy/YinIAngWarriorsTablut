73458f77e51862195dbe31f16d0d40b8515b8767
Nicolò Bartelucci <nicobar97@gmail.com>
2020-04-15 12:13:19 +0200

    Add evalWhite and evalBlack

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/aima/AlphaBetaSearch.kt
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/aima/AlphaBetaSearch.kt
@@ -2,6 +2,8 @@ package it.unibo.ai.didattica.competition.tablut.client.player.aima
 
 import aima.core.search.adversarial.Game
 import aima.core.search.adversarial.IterativeDeepeningAlphaBetaSearch
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.BlackHeuristic
+import it.unibo.ai.didattica.competition.tablut.client.player.heuristic.WhiteHeuristic
 import it.unibo.ai.didattica.competition.tablut.domain.Action
 import it.unibo.ai.didattica.competition.tablut.domain.State
 
@@ -17,17 +19,18 @@ class AlphaBetaSearch(game: Game<State, Action, State.Turn>?, utilMin: Double, u
      * @return evaluation
      */
     override fun eval(state: State, turn: State.Turn): Double {
+        super.eval(state, turn)
         if (game.isTerminal(state))
             return game.getUtility(state, turn)
         return if (turn == State.Turn.BLACK) evalBlack(state) else evalWhite(state)
     }
 
     private fun evalBlack(state: State): Double {
-        return Double.NEGATIVE_INFINITY
+        return BlackHeuristic.genericBlackEval(state)
     }
 
     private fun evalWhite(state: State): Double {
-        return Double.NEGATIVE_INFINITY
+        return WhiteHeuristic.genericWhiteEval(state)
     }
     
 }
