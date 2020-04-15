ff91526c680d941bdf965405677ea32ace00ae10
Nicolò Bartelucci <nicobar97@gmail.com>
2020-04-15 12:15:30 +0200

    Edit to set timeout for white and black

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/TablutPlayerLauncher.kt
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/client/player/TablutPlayerLauncher.kt
@@ -2,6 +2,9 @@ package it.unibo.ai.didattica.competition.tablut.client.player
 
 fun main(args: Array<String>) {
     val role = args.getOrElse(0) { print("Insert player role: "); readLine()!!}
-    val player = TablutPlayer(role, "player")
+    val player = if(role == "white")
+        TablutPlayer(role, "playerWHITE", 59)
+    else
+        TablutPlayer(role, "playerBLACK", 59)
     player.run()
 }
