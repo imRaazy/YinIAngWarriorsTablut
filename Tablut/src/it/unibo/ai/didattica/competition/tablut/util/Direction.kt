e75b96beac17d2c319edaa8de80b5053020a79ab
imRaazy <milo.marchetti@libero.it>
2020-04-20 15:27:58 +0200

    Remove getDirection function

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/util/Direction.kt
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/util/Direction.kt
@@ -1,16 +1,5 @@
 package it.unibo.ai.didattica.competition.tablut.util
 
 enum class Direction() {
-    UP, DOWN, LEFT, RIGHT, NONE;
-    companion object {
-        fun fromValue(value: Int): Direction {
-            return when (value) {
-                -1 -> LEFT
-                0 -> UP
-                1 -> RIGHT
-                2 -> DOWN
-                else -> NONE
-            }
-        }
-    }
+    UP, DOWN, LEFT, RIGHT
 }
