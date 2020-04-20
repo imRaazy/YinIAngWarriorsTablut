b7fe82720f7eef44b6cc940cadf06f354ce2e50c
Nicolò Bartelucci <nicobar97@gmail.com>
2020-04-20 12:36:06 +0200

    Add 2 more directions and typo fix

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/util/Direction.kt
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/util/Direction.kt
@@ -3,10 +3,12 @@ package it.unibo.ai.didattica.competition.tablut.util
 enum class Direction() {
     UP, DOWN, LEFT, RIGHT, NONE;
     companion object {
-        fun formValue(value: Int): Direction {
+        fun fromValue(value: Int): Direction {
             return when (value) {
                 -1 -> LEFT
+                0 -> UP
                 1 -> RIGHT
+                2 -> DOWN
                 else -> NONE
             }
         }
