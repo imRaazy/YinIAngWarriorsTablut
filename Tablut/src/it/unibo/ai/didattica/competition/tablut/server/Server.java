a6c905937feece595b9f20def65d3891cb4e6d53
Nicolò Bartelucci <nicobar97@gmail.com>
2020-04-21 11:42:13 +0200

    Increase server timeout

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/server/Server.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/server/Server.java
@@ -97,7 +97,7 @@ public class Server implements Runnable {
 	public Server(int timeout, int cacheSize, int numErrors, int repeated, int game, boolean gui) {
 		this.gameC = game;
 		this.enableGui = gui;
-		this.time = timeout;
+		this.time = 900000;
 		this.moveCache = cacheSize;
 		this.errors = numErrors;
 		this.cacheSize = cacheSize;
